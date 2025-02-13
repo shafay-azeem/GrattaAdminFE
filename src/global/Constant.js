d77f89b525ac6f88f05f90a7c3a5451f2508dafd
shafay-azeem <shafayazeem12@gmail.com>
2025-02-14 00:52:27 +0100

    updated

--- src/global/Constant.js
+++ src/global/Constant.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "http://localhost:7000/";
-// export const BASE_URL = "https://gratta-admin-be-2.vercel.app/";
+// /export const BASE_URL = "http://localhost:7000/";
+export const BASE_URL = "https://gratta-admin-be-2.vercel.app/";
+
 
 export const API_URL = {
   SIGNUP: "api/user/V1/createUser",
